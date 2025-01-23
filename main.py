f0eef9123136d2be05c4ec65fddbe11b7467a283
Anthony <91166746+anthonyjV1@users.noreply.github.com>
2025-01-22 19:34:48 -0500

    creating labels and normalizing dataset

--- main.py
+++ main.py
@@ -80,4 +80,74 @@ class Dataset(object):
         return ConcatDataset([self, other])
     
 class MRI(Dataset):
-    pass
+    def __init__(self):
+        # Prepare images
+        oa_severe = []
+        oa_moderate = []
+        oa_minimal = []
+        oa_healthy = []
+        oa_doubtful = []
+
+        def data(x, y):
+            path = x
+            for i in glob.iglob(path):
+                img = cv2.imread(i)
+                # Resize images to (128, 128)
+                img = cv2.resize(img, (128, 128))
+                # Convert BGR to RGB
+                b, g, r = cv2.split(img)
+                img = cv2.merge([r, g, b])
+                y.append(img)
+
+        # Load images into respective lists
+        data('./dataset/Doubtful/*.jpg', oa_doubtful)
+        data('./dataset/Healthy/*.jpg', oa_healthy)
+        data('./dataset/Minimal/*.jpg', oa_minimal)
+        data('./dataset/Moderate/*.jpg', oa_moderate)
+        data('./dataset/Severe/*.jpg', oa_severe)
+
+        # Convert lists to numpy arrays
+        oa_severe = np.array(oa_severe, dtype=np.float32)
+        oa_moderate = np.array(oa_moderate, dtype=np.float32)
+        oa_minimal = np.array(oa_minimal, dtype=np.float32)
+        oa_healthy = np.array(oa_healthy, dtype=np.float32)
+        oa_doubtful = np.array(oa_doubtful, dtype=np.float32)
+
+        # Prepare labels
+        label_mapping = {
+            'Doubtful': 0,
+            'Healthy': 1,
+            'Minimal': 2,
+            'Moderate': 3,
+            'Severe': 4
+        }
+
+        labels_doubtful = np.full(len(oa_doubtful), label_mapping['Doubtful'], dtype=np.int64)
+        labels_healthy = np.full(len(oa_healthy), label_mapping['Healthy'], dtype=np.int64)
+        labels_minimal = np.full(len(oa_minimal), label_mapping['Minimal'], dtype=np.int64)
+        labels_moderate = np.full(len(oa_moderate), label_mapping['Moderate'], dtype=np.int64)
+        labels_severe = np.full(len(oa_severe), label_mapping['Severe'], dtype=np.int64)
+
+        # Concatenate all images and labels
+        all_images = np.concatenate((oa_doubtful, oa_healthy, oa_minimal, oa_moderate, oa_severe))
+        all_labels = np.concatenate((labels_doubtful, labels_healthy, labels_minimal, labels_moderate, labels_severe))
+
+        # Normalize images
+        all_images = all_images / 255.0  # Normalize pixel values to [0, 1]
+
+        # Assign to instance variables
+        self.images = all_images
+        self.labels = all_labels
+
+    def __len__(self):
+        return self.images.shape[0]
+
+    def __getitem__(self, index):
+        sample = {'image': self.images[index], 'label': self.labels[index]}
+        return sample
+
+    
+
+mri = MRI()
+img = mri[5]['image']
+print(np.max(img))
